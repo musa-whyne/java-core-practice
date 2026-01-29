e015b5c35c8dd9dbc8fd44f6b20341253e993d70
Muhammad Musa Ishaq Whyne <114241733+musa-whyne@users.noreply.github.com>
2026-01-29 11:30:21 +0500

    Add Main class to demonstrate shape areas

---
+++ oop/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        System.out.println("Circle Area: " + circle.area());
+        System.out.println("Rectangle Area: " + rectangle.area());
+    }
+}
